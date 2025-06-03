9cdd2763b36525933e7fdcea0b379d9cbcdf6c79
rolindaz <rolinda.zamboni@gmail.com>
2025-06-03 14:39:22 +0200

    Refactor Palindroma game logic and improve code readability

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -5,33 +5,44 @@ Chiedere all’utente di inserire una parola. Creare una funzione per capire se 
 
 // let userWord = prompt('Inserisci una parola');
 
-// Creare una funzione per capire se la parola inserita è palindroma
 
+// Creare una funzione per capire se la parola inserita è palindroma
 // Come capisco se una parola è palindroma? La versione normale di quella parola dev'essere uguale alla versione capovolta. Dovrò quindi creare due variabili, una con la parola normale e una con la parola capovolta.
 
 // let word = 'paprika', reversedWord = '';
 
+
 // La variabile della parola capovolta dev'essere ottenuta invertendo l'ordine in cui si trovano le lettere che la compongono.
 
-/* for (let i = word.length; i >= 0; i--) {
+/* 
+
+for (let i = word.length; i >= 0; i--) {
     let currentLetter = word.charAt(i);
     reversedWord = reversedWord + currentLetter;
-    }; */
+};
+
+*/
+
 
 // Per verificare se la parola originaria è palindroma, devo verificare l'uguaglianza tra word e reversedWord:
 
-/* if (word == reversedWord) {
+/* 
+
+if (word == reversedWord) {
     console.log(alert('Questa parola è un palindromo!'));
 } else {
     console.log(alert('Questa parola non è un palindromo!'));
-}; */
+}; 
+
+*/
+
 
 // Porto quindi questa logica all'interno della funzione:
 
 let reversedString = '';
 
 function verifyPalyndrome(string) {
-    palyndrome = 'Questa parola è un palindromo!', notPalyndrome = 'Questa parola non è un palindromo!';
+    let palyndrome = 'Questa parola è un palindromo!', notPalyndrome = 'Questa parola non è un palindromo!';
     for (let i = string.length; i >= 0; i--) {
         let currentLetter = string.charAt(i);
         reversedString = reversedString + currentLetter;
@@ -50,11 +61,3 @@ let userWord = prompt('Inserisci una parola');
 // Invoco la funzione con il parametro ottenuto dal prompt
 
 console.log(verifyPalyndrome(userWord));
-
-
-
-
-
-
-
-/* Pari e Dispari L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
